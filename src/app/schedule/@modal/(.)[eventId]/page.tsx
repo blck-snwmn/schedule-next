57f42cf6ad05ca298db3b4055298bcec3dab6964
blck-snwmn <whitesnowmancreator@gmail.com>
2024-07-21 21:59:51 +0900

    Add dialog

--- src/app/schedule/@modal/(.)[eventId]/page.tsx
+++ src/app/schedule/@modal/(.)[eventId]/page.tsx
@@ -1,9 +1,28 @@
+"use client"
+
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { useRouter } from "next/navigation"
 
 type Props = {
     params: { eventId: string }
 }
-export default function Detail({ params }: Props) {
+export default async function Detail({ params }: Props) {
+    const router = useRouter(); // required "use client"
     return (
-        <div>app/schedule/@modal/{params.eventId}/page.tsx</div>
+        <Dialog open={true} onOpenChange={() => router.back()}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Are you absolutely sure?</DialogTitle>
+                    <DialogDescription>
+                        <div>app/schedule/@modal/{params.eventId}/page.tsx</div>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </DialogDescription>
+                    <DialogFooter>
+                        footer
+                    </DialogFooter>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     )
 }
