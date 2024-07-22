bfb0ff0f2beb136825e9c5d81f0a2bdb843a8a9c
blck-snwmn <whitesnowmancreator@gmail.com>
2024-07-22 09:05:36 +0900

    Fix

--- src/components/schedule.tsx
+++ src/components/schedule.tsx
@@ -77,10 +77,11 @@ const TalentSelector: React.FC<{
 		<div className="flex flex-wrap mb-4 gap-2">
 			<button
 				type="button"
-				className={`px-4 py-2 rounded transition-colors duration-200 ${!selectedTalent
+				className={`px-4 py-2 rounded transition-colors duration-200 ${
+					!selectedTalent
 						? "bg-blue-600 text-white"
 						: "bg-gray-700 text-gray-200 hover:bg-gray-600"
-					}`}
+				}`}
 				onClick={() => onSelect(null)}
 			>
 				All
@@ -89,10 +90,11 @@ const TalentSelector: React.FC<{
 				<button
 					type="button"
 					key={talent.id}
-					className={`px-4 py-2 rounded transition-colors duration-200 ${selectedTalent?.id === talent.id
+					className={`px-4 py-2 rounded transition-colors duration-200 ${
+						selectedTalent?.id === talent.id
 							? "bg-blue-600 text-white"
 							: "bg-gray-700 text-gray-200 hover:bg-gray-600"
-						}`}
+					}`}
 					onClick={() => onSelect(talent)}
 				>
 					{talent.name}
