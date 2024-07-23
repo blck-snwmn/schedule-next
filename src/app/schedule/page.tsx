c264a5d2c352d240491bd778e3ca48547871fc45
blck-snwmn <whitesnowmancreator@gmail.com>
2024-07-23 22:32:46 +0900

    Rename page

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,7 @@
 import { Events } from "@/components/schedule";
 import { getEvents, getTaletns } from "@/services/getData";
 
-export default async function Home() {
+export default async function SchedulePage() {
 	const scheduleEvent = await getEvents();
 	const talents = await getTaletns();
 	return <Events scheduleEvent={scheduleEvent} talents={talents} />;
