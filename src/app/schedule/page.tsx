ee912cc190ad173d68d6539ae7bd898fc5f1fde5
blck-snwmn <whitesnowmancreator@gmail.com>
2024-07-24 02:46:53 +0900

    feat: Add date validation to SchedulePage component

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,7 +1,25 @@
 import { Events } from "@/components/schedule";
 import { getEvents, getTaletns } from "@/services/getData";
+import { isValid, parse } from "date-fns";
+import { notFound } from "next/navigation";
 
-export default async function SchedulePage() {
+type Props = {
+	searchParams: { year?: string; month?: string };
+};
+
+export default async function SchedulePage({ searchParams }: Props) {
+	// `month` and `year` are obtained from URL parameters
+	// If not set as a URL parameter, the current year and month are used. 
+	const now = new Date();
+	const year = searchParams.year || now.getFullYear().toString();
+	const month = searchParams.month || (now.getMonth() + 1).toString();
+
+	// FIXME: result is UTC
+	const result = parse(`${year}-${month}`, "yyyy-MM", new Date());
+	if (!isValid(result)) {
+		notFound();
+	}
+
 	const scheduleEvent = await getEvents();
 	const talents = await getTaletns();
 	return <Events scheduleEvent={scheduleEvent} talents={talents} />;
