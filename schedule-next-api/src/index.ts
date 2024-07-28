564b3cee343a9a962275f4f4d0061dc389cf2fc6
blck-snwmn <whitesnowmancreator@gmail.com>
2024-07-28 20:32:21 +0900

    Add

--- schedule-next-api/src/index.ts
+++ schedule-next-api/src/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
 import { events, eventTalents, schedules, talents } from "./schema";
 import { and, eq, gte, lte } from "drizzle-orm";
-import { EventWithDetails, QueryResult } from "./types";
+import { EventWithDetails, NewEvent, NewSchedule, NewTalent, QueryResult } from "./types";
 
 const app = new Hono<{ Bindings: Env }>();
 
@@ -10,10 +10,14 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.get('/api/events', async (c) => {
 	const { year, month } = c.req.query();
+	const now = new Date();
+	const yearStr = year || now.getFullYear().toString();
+	const monthStr = month || (now.getMonth() + 1).toString();
+
 	const db = drizzle(c.env.DB);
 
-	const startOfMonth = new Date(parseInt(year), parseInt(month) - 1, 1);
-	const endOfMonth = new Date(parseInt(year), parseInt(month), 0, 23, 59, 59);
+	const startOfMonth = new Date(parseInt(yearStr), parseInt(monthStr) - 1, 1);
+	const endOfMonth = new Date(parseInt(yearStr), parseInt(monthStr), 0, 23, 59, 59);
 
 	const result = await db.select({
 		id: events.id,
@@ -35,6 +39,7 @@ app.get('/api/events', async (c) => {
 			)
 		) as QueryResult[];
 
+	console.log(result.length);
 	// 結果を整形
 	const formattedEvents: EventWithDetails[] = result.reduce((acc: EventWithDetails[], curr: QueryResult) => {
 		const eventIndex = acc.findIndex(e => e.id === curr.id);
@@ -64,42 +69,56 @@ app.get('/api/talents', async (c) => {
 	return c.json(result);
 });
 
-// 新しいイベントを作成するエンドポイントの例
+app.post('/api/talents', async (c) => {
+	const db = drizzle(c.env.DB);
+	const talentData = await c.req.json();
+
+	const newTalent: NewTalent = {
+		id: crypto.randomUUID(),
+		name: talentData.name,
+	};
+
+	await db.insert(talents).values(newTalent);
+
+	return c.json({ id: newTalent.id, name: newTalent.name }, 201);
+});
+
+// イベント登録エンドポイント
 app.post('/api/events', async (c) => {
-	// const db = drizzle(c.env.DB);
-	// const eventData = await c.req.json();
+	const db = drizzle(c.env.DB);
+	const eventData = await c.req.json();
 
-	// const newEvent = {
-	// 	id: crypto.randomUUID(), // UUIDを生成
-	// 	name: eventData.name,
-	// 	description: eventData.description,
-	// 	category: eventData.category,
-	// 	thumbnail: eventData.thumbnail,
-	// };
+	const newEvent: NewEvent = {
+		id: crypto.randomUUID(),
+		name: eventData.name,
+		description: eventData.description,
+		category: eventData.category,
+		thumbnail: eventData.thumbnail,
+	};
 
-	// await db.insert(events).values(newEvent);
+	await db.insert(events).values(newEvent);
 
-	// // スケジュールの追加
-	// for (const scheduleData of eventData.schedules) {
-	// 	await db.insert(schedules).values({
-	// 		id: crypto.randomUUID(),
-	// 		eventId: newEvent.id,
-	// 		name: scheduleData.name,
-	// 		startAt: new Date(scheduleData.startAt),
-	// 		endAt: new Date(scheduleData.endAt),
-	// 	});
-	// }
+	// スケジュールの追加
+	for (const scheduleData of eventData.schedules) {
+		const newSchedule: NewSchedule = {
+			id: crypto.randomUUID(),
+			eventId: newEvent.id,
+			name: scheduleData.name,
+			startAt: new Date(scheduleData.startAt),
+			endAt: new Date(scheduleData.endAt),
+		};
+		await db.insert(schedules).values(newSchedule);
+	}
 
-	// // タレントの関連付け
-	// for (const talentId of eventData.talentIds) {
-	// 	await db.insert(eventTalents).values({
-	// 		id: crypto.randomUUID(),
-	// 		eventId: newEvent.id,
-	// 		talentId: talentId,
-	// 	});
-	// }
+	// タレントの関連付け
+	for (const talentId of eventData.talentIds) {
+		await db.insert(eventTalents).values({
+			eventId: newEvent.id,
+			talentId: talentId,
+		});
+	}
 
-	// return c.json({ id: newEvent.id }, 201);
+	return c.json({ id: newEvent.id }, 201);
 });
 
 export default app;
