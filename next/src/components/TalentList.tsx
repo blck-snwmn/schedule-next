4b991f2ef3c500d0967f6feecf1bf6e642a53700
blck-snwmn <whitesnowmancreator@gmail.com>
2024-08-03 18:58:25 +0900

    Set width

--- next/src/components/TalentList.tsx
+++ next/src/components/TalentList.tsx
@@ -40,13 +40,16 @@ export const talentColumns: ColumnDef<Talent>[] = [
 		accessorKey: "name",
 		header: ({ column }) => {
 			return (
-				<Button
-					variant="ghost"
-					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-				>
-					Name
-					<ArrowUpDown className="ml-2 h-4 w-4" />
-				</Button>
+				<div className="w-60">
+					<Button
+						variant="ghost"
+						onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+						className="-ml-3"
+					>
+						Name
+						<ArrowUpDown className="ml-2 h-4 w-4" />
+					</Button>
+				</div>
 			);
 		},
 	},
