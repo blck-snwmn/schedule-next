4e82227a1781636c3d7956fc8ea9cec8564ffbd9
blck-snwmn <whitesnowmancreator@gmail.com>
2024-08-04 18:20:04 +0900

    Add default value

--- next/src/app/admin/events/page.tsx
+++ next/src/app/admin/events/page.tsx
@@ -1,8 +1,31 @@
 import { EventList } from "@/components/AdminEventList"
 import { getEvents } from "@/services/getData"
+import { isValid, parseISO } from "date-fns";
+import { notFound } from "next/navigation";
 
-export default async function Event() {
-    const events = await getEvents(2024, 7)
+type Props = {
+    searchParams: { year?: string; month?: string };
+};
+
+export default async function Event({ searchParams }: Props) {
+    // `month` and `year` are obtained from URL parameters
+    // If not set as a URL parameter, the current year and month are used.
+    const now = new Date();
+    const yearStr = searchParams.year || now.getFullYear().toString();
+    const monthStr = searchParams.month || (now.getMonth() + 1).toString();
+
+    const paddedMonth = monthStr.padStart(2, "0");
+
+    // FIXME: result is UTC
+    const result = parseISO(`${yearStr}-${paddedMonth}-01T00:00:00Z`);
+    console.info(yearStr, monthStr, result);
+    if (!isValid(result)) {
+        notFound();
+    }
+    const year = result.getFullYear();
+    const month = result.getMonth() + 1;
+
+    const events = await getEvents(year, month)
     return (
         <div className="space-y-6">
             <div className="flex justify-between items-center">
