b13d88b1c95d476a334bf2ba1b12ef0191475c74
blck-snwmn <whitesnowmancreator@gmail.com>
2024-08-04 22:04:34 +0900

    Add move month link

--- next/src/app/admin/events/page.tsx
+++ next/src/app/admin/events/page.tsx
@@ -1,6 +1,7 @@
 import { EventList } from "@/components/AdminEventList"
 import { getEvents } from "@/services/getData"
 import { isValid, parseISO } from "date-fns";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -25,11 +26,37 @@ export default async function Event({ searchParams }: Props) {
     const year = result.getFullYear();
     const month = result.getMonth() + 1;
 
+    const prevMonth = new Date(year, month - 2);
+    const nextMonth = new Date(year, month);
+
+    const formatMonth = (date: Date) =>
+        (date.getMonth() + 1).toString().padStart(2, "0");
+
+
     const events = await getEvents(year, month)
     return (
         <div className="space-y-6">
             <div className="flex justify-between items-center">
                 <h1 className="text-3xl font-bold tracking-tight">イベント管理</h1>
+            </div>
+            <div>
+                <Link
+                    href={`/admin/events?year=${prevMonth.getFullYear()}&month=${formatMonth(prevMonth)}`}
+                    className="mr-2"
+                >
+                    前月
+                </Link>
+                <Link
+                    href={`/admin/events?year=${now.getFullYear()}&month=${formatMonth(now)}`}
+                    className="mr-2"
+                >
+                    今月
+                </Link>
+                <Link
+                    href={`/admin/events?year=${nextMonth.getFullYear()}&month=${formatMonth(nextMonth)}`}
+                >
+                    翌月
+                </Link>
             </div>
             <EventList events={events} />
         </div>
