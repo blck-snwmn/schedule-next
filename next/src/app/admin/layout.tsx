143c66350522045d84fe4be2f0c913d6da58abe6
blck-snwmn <whitesnowmancreator@gmail.com>
2024-08-12 06:59:52 +0900

    Fix lint

--- next/src/app/admin/layout.tsx
+++ next/src/app/admin/layout.tsx
@@ -1,11 +1,5 @@
 import AdminSidebar from "@/components/AdminSidebar";
 
-export default function Layout({
-	children,
-}: { children: React.ReactNode }) {
-	return (
-		<AdminSidebar>
-			{children}
-		</AdminSidebar>
-	);
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return <AdminSidebar>{children}</AdminSidebar>;
 }
