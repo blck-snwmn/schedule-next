705812a95bf8bd437be05f9152774db876924029
blck-snwmn <whitesnowmancreator@gmail.com>
2024-08-20 01:29:41 +0900

    Move Category

--- next/src/components/CategoryBadge.tsx
+++ next/src/components/CategoryBadge.tsx
@@ -1,23 +1,5 @@
+import { categoryColors, type Category } from "@/services/type";
 import { Badge } from "./ui/badge";
-
-type Category =
-	| "音楽"
-	| "ライブ"
-	| "メディア"
-	| "出版"
-	| "ファンイベント"
-	| "コラボレーション";
-
-const categoryColors: Record<Category, string> = {
-	音楽: "bg-blue-500",
-	ライブ: "bg-green-500",
-	メディア: "bg-purple-500",
-	出版: "bg-yellow-500",
-	ファンイベント: "bg-pink-500",
-	コラボレーション: "bg-indigo-500",
-};
-
-const categories: Category[] = Object.keys(categoryColors) as Category[];
 
 const getCategoryColor = (category: string): string => {
 	if (isCategory(category)) {
--- next/src/services/type.ts
+++ next/src/services/type.ts
@@ -1,6 +1,27 @@
 import type { createEventSchema, eventSchema, updateEventSchema } from "schema";
 import type { z } from "zod";
 
+export type Category =
+    | "音楽"
+    | "ライブ"
+    | "メディア"
+    | "出版"
+    | "ファンイベント"
+    | "コラボレーション"
+    | "その他";
+
+export const categoryColors: Record<Category, string> = {
+    音楽: "bg-blue-500",
+    ライブ: "bg-green-500",
+    メディア: "bg-purple-500",
+    出版: "bg-yellow-500",
+    ファンイベント: "bg-pink-500",
+    コラボレーション: "bg-indigo-500",
+    その他: "bg-gray-500",
+};
+
+export const categories: Category[] = Object.keys(categoryColors) as Category[];
+
 export type ScheduleEvent = z.infer<typeof eventSchema>;
 export type Schedule = z.infer<typeof eventSchema.shape.schedules.element>;
 export type CreateScheduleEvent = z.infer<typeof createEventSchema>;
