87bb4788c89c0b1b96a3c9d19693eea61b1fbc44
blck-snwmn <whitesnowmancreator@gmail.com>
2024-08-25 02:30:10 +0900

    Remove comment

--- next/src/services/schema.ts
+++ next/src/services/schema.ts
@@ -13,19 +13,6 @@ const scheduleSchema = z.object({
 	endAt: z.string().transform((str) => new Date(str)),
 });
 
-// export const eventFormSchema = z.object({
-// 	id: z.string().uuid().optional(),
-// 	name: z.string().min(1, "Event name is required"),
-// 	category: z.string().min(1, "Category is required"),
-// 	description: z.string().optional(),
-// 	thumbnail: z.string().url().optional(),
-// 	talentIds: z.array(z.string().uuid()),
-// 	schedules: z
-// 		.array(scheduleSchema)
-// 		.min(1, "At least one schedule is required"),
-// });
-
-// export const eventFormSchema = updateEventSchema.or(createEventSchema);
 export const eventFormSchema = createEventSchema
 	.merge(updateEventSchema)
 	.partial({ id: true });
@@ -33,7 +20,3 @@ export const eventFormSchema = createEventSchema
 export const groupFormSchema = createGroupSchema
 	.merge(updateGroupSchema)
 	.partial({ id: true });
-// export const eventFormSchema = createEventSchema.or(updateEventSchema);
-type zzz1 = z.infer<typeof updateEventSchema>;
-type zzz2 = z.infer<typeof createEventSchema>;
-type zzz3 = z.infer<typeof eventFormSchema>;
